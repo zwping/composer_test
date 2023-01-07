d8d3337e0c17955c1700b53653d2c033da3651d7
zwping <1101558280@qq.com>
2023-01-07 14:53:45 +0800

    1

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+use Zwping\ComposerTest\Test;
+
+require __DIR__ . '/vendor/autoload.php';
+
+
+
+
+$test = new Test();
+$test->test();
---
+++ src/Test.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Zwping\ComposerTest;
+
+class Test {
+    public function test() {
+        echo 'hi' . PHP_EOL;
+    }
+}
